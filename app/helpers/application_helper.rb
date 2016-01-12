86ed5b495288e99e60002cb287c7dfe9e3f5e2c2
vonbarg <vonbarg@us.ibm.com>
2016-01-12 10:45:21 -0600

    Fixing up Ruby REST app - part 2 - use cookies so that same listener session is used throughout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ require "uri"
 module ApplicationHelper
 
 	# To run locally, set URL, USER, DBNAME, and PASSWORD fields here for REST connectivity
-	# For example, URL = "http://localhost:27108/testdb"
 	URL = ""
 	DBNAME = ""
 	USER = ""
@@ -61,13 +60,15 @@ module ApplicationHelper
 		
 		# check if collection exists, if so delete
 		http = Net::HTTP.new(uri.host, uri.port)
-		request = Net::HTTP::Get.new(uri.request_uri)
+		request = Net::HTTP::Get.new(uri + "#{dbname}")
 		request.basic_auth(user, password)
 		response = http.request(request)
+		cookie = response['set-cookie'].split('; ')
 		output.push("Existing collections: #{response.body}")
 		if response.body.include? "#{collectionName}"
 			output.push("Deleting collection: #{collectionName}")
 			request = Net::HTTP::Delete.new(uri + "/#{dbname}/#{collectionName}")
+			request['cookie'] = cookie
 			response = http.request(request)
 			unless code_2xx?(response.code)
 				output.push("Failed to delete existing collection")
@@ -76,9 +77,9 @@ module ApplicationHelper
 
 		# create collection
 		output.push("Creating new collection: #{collectionName}")
-		request = Net::HTTP::Post.new(uri)
-		request.basic_auth user, password
+		request = Net::HTTP::Post.new(uri + "#{dbname}")
 		request.content_type = 'application/json'
+		request['cookie'] = cookie
 		request.body = {:name => "#{collectionName}"}.to_json
 		response = http.request(request)
 		unless code_2xx?(response.code)
@@ -88,8 +89,8 @@ module ApplicationHelper
 		output.push("Insert a single document to a collection")
 		data = {:name => "test1",:value => "1"}.to_json
 		request = Net::HTTP::Post.new(uri + "#{dbname}/#{collectionName}")
-		request.basic_auth user, password
 		request.content_type = 'application/json'
+		request['cookie'] = cookie
 		request.body = data
 		response = http.request(request)
 		unless code_2xx?(response.code)
@@ -99,8 +100,8 @@ module ApplicationHelper
 		output.push("Insert multiple documents to a collection")
 		data = [{:name => 'test1', :value => 101}, {:name => 'test2', :value => 202}, {:name => 'test3', :value => 303}].to_json
 		request = Net::HTTP::Post.new(uri + "/#{dbname}/#{collectionName}")
-		request.basic_auth user, password
 		request.content_type = 'application/json'
+		request['cookie'] = cookie
 		request.body = data
 		response = http.request(request)
 		unless code_2xx?(response.code)
@@ -111,8 +112,8 @@ module ApplicationHelper
 		output.push("Find a document in a collection that matches a query condition")
 		queryStr = '{"name":"test1"}'
 		request = Net::HTTP::Get.new(uri + "/#{dbname}/#{collectionName}?query=#{queryStr}&fields={_id:0}&limit=1")
-		request.basic_auth user, password
 		request.content_type = 'application/json'
+		request['cookie'] = cookie
 		response = http.request(request)
 		output.push("Result of query: #{response.body}")
 
@@ -120,15 +121,15 @@ module ApplicationHelper
 		output.push("Find all documents in a collection that match a query condition")
 		queryStr = '{"name":"test1"}'
 		request = Net::HTTP::Get.new(uri + "/#{dbname}/#{collectionName}?query=#{queryStr}&fields={_id:0}")
-		request.basic_auth user, password
 		request.content_type = 'application/json'
+		request['cookie'] = cookie
 		response = http.request(request)
 		output.push("Result of query: #{response.body}")
 
 		output.push(" ")
 		output.push("Find all documents in a collection")
 		request = Net::HTTP::Get.new(uri + "/#{dbname}/#{collectionName}")
-		request.basic_auth user, password
+		request['cookie'] = cookie
 		response = http.request(request)
 		output.push("All documents in collection:  #{response.body}")
 
@@ -137,8 +138,8 @@ module ApplicationHelper
 		queryStr = '{name:"test3"}'
 		updateStr = '{"$set" : {"value" : "9"} }'
 		request = Net::HTTP::Put.new(uri + "/#{dbname}/#{collectionName}?query=#{queryStr}")
-		request.basic_auth user, password
 		request.content_type = 'application/json'
+		request['cookie'] = cookie
 		request.body = updateStr
 		response = http.request(request)
 		unless code_2xx?(response.code)
@@ -149,8 +150,8 @@ module ApplicationHelper
 		output.push("Delete documents in a collection")
 		queryStr = '{name:"test2"}'
 		request = Net::HTTP::Delete.new(uri + "/#{dbname}/#{collectionName}?query=#{queryStr}")
-		request.basic_auth user, password
 		request.content_type = 'application/json'
+		request['cookie'] = cookie
 		response = http.request(request)
 		unless code_2xx?(response.code)
 			output.push("Failed to delete document")
@@ -158,8 +159,8 @@ module ApplicationHelper
 
 		output.push(" ")
 		output.push("Get a listing of collections")
-		request = Net::HTTP::Get.new(uri)
-		request.basic_auth user, password
+		request = Net::HTTP::Get.new(uri + "#{dbname}")
+		request['cookie'] = cookie
 		response = http.request(request)
 		if code_2xx?(response.code)
 			output.push("Existing collections: #{response.body}")
@@ -170,7 +171,7 @@ module ApplicationHelper
 		output.push(" ")
 		output.push("Drop collection: #{collectionName}")
 		request = Net::HTTP::Delete.new(uri + "/#{dbname}/#{collectionName}")
-		request.basic_auth user, password
+		request['cookie'] = cookie
 		response = http.request(request)
 		unless code_2xx?(response.code)
 			output.push("Failed to drop collection")
